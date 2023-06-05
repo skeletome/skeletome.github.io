25583a9c0250e8538a1d0bc1728be45324dbcdef
skeletome <50918151+skeletome@users.noreply.github.com>
2023-06-05 23:57:35 +0700

    Add files via upload

--- contact.js
+++ contact.js
@@ -8,16 +8,10 @@ const findUs = document.querySelector('.find-us li a');
 const listfindUs = document.querySelector('.find-us .list-findus');
 const downList = document.querySelector('.isi-kanan .find-us > li > a >img');
 const nav = document.querySelector('nav');
-const blue = document.querySelector('.blue');
 const footer = document.querySelector('.footer');
-const vitamin = document.querySelector('.alpavit');
-const buy = document.querySelector('.section-buy');
-const gambarKandungan = document.querySelector('.alpavit .alpavit-wrapper .wrapper-kiri img');
-const h1Kandungan = document.querySelector('.alpavit .alpavit-wrapper .wrapper-tengah h1');
-const pKandungan = document.querySelector('.alpavit .alpavit-wrapper .wrapper-tengah p')
-const tombolKandungan = document.querySelector('.alpavit .alpavit-wrapper .wrapper-kanan button');
 const tombolsosmedFooter = document.querySelector('.footer2-kiri > ul > li > button');
 const wrapsosmedFooter = document.querySelector('.footer2-kiri > ul > div')
+const kontak = document.querySelector('.contact-us');
 
 tombolsosmedFooter.addEventListener ('click', function() {
   if (wrapsosmedFooter.style.display == 'none'|| !wrapsosmedFooter.style.display) {
@@ -72,10 +66,10 @@ navMenu.addEventListener ('click', function() {
    navmenuIsi.style.transform ='translateY(0px)';
    navmenuIsi.style.transition = 'transform .5s ease'
    
-   blue.style.display = 'none';
+ 
    footer.style.display = 'none';
-   vitamin.style.display = 'none';
-   buy.style.display = 'none';
+   kontak.style.display = 'none';
+   
    
   } 
   else {
@@ -91,10 +85,10 @@ navMenu.addEventListener ('click', function() {
      tulisanMenu.innerHTML = 'Menu';
      gambarMenu.src = 'images/logo.svg'
      iconMenu.src = 'images/menu.svg'
-     blue.style.display = 'flex';
+     
      footer.style.display = 'flex';
-     vitamin.style.display = 'flex';
-     buy.style.display = 'flex';
+     kontak.style.display = 'flex';
+     
 
      tomboliconMenu.style.animation ='fadeIn .8s ease-in-out';
    };
@@ -116,41 +110,6 @@ navMenu.addEventListener ('click', function() {
 
 });
 
-tombolKandungan.addEventListener('click', function() {
-  if (gambarKandungan.src.includes ('images/alpaarbutin.svg')){
-    gambarKandungan.src = 'images/vitaminc.svg';
-    h1Kandungan.innerHTML = ' Vitamin C';
-    pKandungan.innerHTML = ' Vitamin C is a well-established ingredient in the world of skincare. Its potent antioxidant properties protect the skin from harmful free radicals, which can cause premature aging and skin damage. In addition to its protective qualities, Vitamin C is renowned for its ability to brighten the complexion, fade dark spots, and even out skin tone. By promoting collagen production.';
-
-    gambarKandungan.style.animation = 'fadeIn 1s ease-in';
-    h1Kandungan.style.animation = 'fadeIn .5s ease-in';
-    pKandungan.style.animation = 'fadeIn .5s ease-in';
-
-    function hapusanimasi () {
-    gambarKandungan.style.animation = '';
-    h1Kandungan.style.animation = '';
-    pKandungan.style.animation = '';
-    }
-    setTimeout(hapusanimasi, 600)
-  }
-
-  else {
-    gambarKandungan.src = 'images/alpaarbutin.svg';
-    h1Kandungan.innerHTML = ' Alpha Arbutin 3%';
-    pKandungan.innerHTML = ' Alpha Arbutin works by inhibiting the production of melanin, the pigment responsible for dark spots, hyperpigmentation, and uneven skin tone. By reducing melanin synthesis, Alpha Arbutin helps fade existing dark spots and prevents the formation of new ones, giving you a more even, radiant complexion. ';
-
-    gambarKandungan.style.animation = 'fadeIn 1s ease-in';
-    h1Kandungan.style.animation = 'fadeIn .5s ease-in';
-    pKandungan.style.animation = 'fadeIn .5s ease-in';
-
-    function hapusanimasi () {
-    gambarKandungan.style.animation = '';
-    h1Kandungan.style.animation = '';
-    pKandungan.style.animation = '';
-    }
-    setTimeout(hapusanimasi, 600)
-  };
-});
 
 
 
